b4fe67e8d2ccbdbc6d2b01b96d2778b62fbfe90f
Musthafa PA <musthafa@softwaysolutions.com>
2023-01-29 17:07:16 +0530

    query function and graphql type definitions

--- server/server.js
+++ server/server.js
@@ -13,6 +13,46 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
+async function query(query) {
+  const result = await pool.query(query);
+  return result[0];
+}
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Course {
+    id: ID!
+    title: String
+    description: String
+    price: Int
+    total_students: Int
+    published: Boolean
+    createdAt: String
+    updatedAt: String
+  }
+  type Query {
+    hello: String
+    getCourses: [Course]
+    getCourseById(id: ID!): Course
+  }
+  type Mutation {
+    createCourses(
+      title: String
+      description: String
+      price: Int
+      total_students: Int
+      published: Boolean
+      createdAt: String
+      updatedAt: String
+    ): Course
+    deleteCourses(id: ID!): String!
+  }
+  type Subscription {
+    courseCreated: [Course]
+    courseDeleted: [Course]
+  }
+`;
+
 const app = express();
 
 app.listen(4000, () => {
