59c71375b147bc98bbd914e622a7b9edf5e6c3d4
Musthafa PA <musthafa@softwaysolutions.com>
2023-01-29 17:26:28 +0530

    graphql http link setup, websocket setup, getting products from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql, useSubscription, useMutation } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+const COURSES = gql`
+  {
+    getCourses {
+      id
+      title
+      description
+      price
+      published
+      total_students
+    }
+  }
+`;
 
 function App() {
+  const [courses, setCourses] = useState();
+  const { data } = useQuery(COURSES);
+
+  useEffect(() => {
+    if (data) {
+      setCourses(data.getCourses);
+    }
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h2>Course Dashboard</h2>
+      </div>
+      <div className="widgets">
+        <div className="course">
+          <h3>List of courses</h3>
+          <ul>
+            {courses &&
+              courses.map((course) => (
+                <li key={course.id}>
+                  {course.id}--------{course.title} ---- {course.price}
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className="create-course"></div>
+        <div className="course-info"></div>
+      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000/graphql",
+    options: {
+      reconnect: true,
+    },
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    ,
   </React.StrictMode>
 );
 
