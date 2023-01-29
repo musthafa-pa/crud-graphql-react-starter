50b2d9370092f938cc90b698587140081a88a754
Musthafa PA <musthafa@softwaysolutions.com>
2023-01-29 17:28:19 +0530

    create course form, mutation added

--- client/src/App.js
+++ client/src/App.js
@@ -16,15 +16,67 @@ const COURSES = gql`
   }
 `;
 
+const CREATE_COURSE = gql`
+  mutation createCourses(
+    $title: String
+    $description: String
+    $published: Boolean
+    $createdAt: String
+    $updatedAt: String
+    $price: Int
+    $total_students: Int
+  ) {
+    createCourses(
+      title: $title
+      description: $description
+      published: $published
+      createdAt: $createdAt
+      updatedAt: $updatedAt
+      price: $price
+      total_students: $total_students
+    ) {
+      title: title
+      description: description
+      price: price
+      total_students: total_students
+      published: published
+      createdAt: createdAt
+      updatedAt: updatedAt
+    }
+  }
+`;
+
 function App() {
   const [courses, setCourses] = useState();
   const { data } = useQuery(COURSES);
+  const [createMutation, { data: createData }] = useMutation(CREATE_COURSE);
 
   useEffect(() => {
     if (data) {
       setCourses(data.getCourses);
     }
   }, [data]);
+
+  const createCourse = (evt) => {
+    evt.preventDefault();
+    let items = document.getElementById("create-course-form").elements;
+    let title = items.title.value;
+    let description = items.description.value;
+    let published = items.publishedOn.checked;
+    let price = Number(items.price.value);
+
+    createMutation({
+      variables: {
+        title: title,
+        description: description,
+        published: published,
+        createdAt: "2022-02-21 16:58:05",
+        updatedAt: "2022-02-21 16:58:05",
+        price: price,
+        total_students: 0,
+      },
+    });
+  };
 
   return (
     <div className="App">
@@ -43,7 +95,55 @@ function App() {
               ))}
           </ul>
         </div>
-        <div className="create-course"></div>
+        <div className="create-course">
+          <h3>Create course</h3>
+
+          <form id="create-course-form">
+            <div class="form-group">
+              <label for="title">Title</label>
+              <input
+                type="text"
+                class="form-control"
+                id="title"
+                placeholder="Enter title"
+              />
+            </div>
+            <div class="form-group">
+              <label for="description">Description</label>
+              <textarea
+                class="form-control"
+                id="description"
+                rows="3"
+              ></textarea>
+            </div>
+            <div class="form-group">
+              <label for="price">Price</label>
+              <input
+                type="text"
+                class="form-control"
+                id="price"
+                placeholder="Enter price"
+              />
+            </div>
+            <div class="form-group form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="publishedOn"
+              />
+              <label class="form-check-label" for="publishedOn">
+                Published On
+              </label>
+            </div>
+            <button
+              type="submit"
+              class="btn btn-primary"
+              onClick={createCourse}
+            >
+              CREATE
+            </button>
+          </form>
+        </div>
         <div className="course-info"></div>
       </div>
     </div>
